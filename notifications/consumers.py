9ad65c773a0d26c40fcb8bc993334391fa51c00e
Obstar005 <obstar300@gmail.com>
2026-03-14 16:56:56 +0000

    Version django à 5.2.12

--- notifications/consumers.py
+++ notifications/consumers.py
@@ -1,30 +1,30 @@
 # notifications/consumers.py
-import json
-from channels.generic.websocket import AsyncWebsocketConsumer
+# import json
+# from channels.generic.websocket import AsyncWebsocketConsumer
 
-class NotificationConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        # Récupère un identifiant d'utilisateur (si connecté sinon 'guest')
-        user = self.scope.get("user")
-        user_id = user.id if user and user.is_authenticated else "guest"
+# class NotificationConsumer(AsyncWebsocketConsumer):
+#     async def connect(self):
+#         # Récupère un identifiant d'utilisateur (si connecté sinon 'guest')
+#         user = self.scope.get("user")
+#         user_id = user.id if user and user.is_authenticated else "guest"
 
-        self.group_name = f"user_{user_id}"
+#         self.group_name = f"user_{user_id}"
 
-        # Rejoint le groupe
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
-        await self.accept()
+#         # Rejoint le groupe
+#         await self.channel_layer.group_add(self.group_name, self.channel_name)
+#         await self.accept()
 
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+#     async def disconnect(self, close_code):
+#         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
-    async def receive(self, text_data):
-        data = json.loads(text_data)
-        await self.send(text_data=json.dumps({
-            'message': f"Message reçu: {data}"
-        }))
+#     async def receive(self, text_data):
+#         data = json.loads(text_data)
+#         await self.send(text_data=json.dumps({
+#             'message': f"Message reçu: {data}"
+#         }))
 
-    async def send_notification(self, event):
-        await self.send(text_data=json.dumps({
-            'message': event["message"]
-        }))
+#     async def send_notification(self, event):
+#         await self.send(text_data=json.dumps({
+#             'message': event["message"]
+#         }))
 
--- notifications/routing.py
+++ notifications/routing.py
@@ -1,6 +1,6 @@
-from django.urls import re_path
-from .consumers import NotificationConsumer
+# from django.urls import re_path
+# from .consumers import NotificationConsumer
 
-websocket_urlpatterns = [
-    re_path(r'ws/notifications/$', NotificationConsumer.as_asgi()),
-]
+# websocket_urlpatterns = [
+#     re_path(r'ws/notifications/$', NotificationConsumer.as_asgi()),
+# ]
